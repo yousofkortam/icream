7bb8be0ffcf4d4a6c43bc3c3828a1f6b59bbe3e0
Yousof Kortam <115800563+yousofkortam@users.noreply.github.com>
2023-03-27 18:04:47 +0200

    make an online database

--- icream/Models/icreamContext.cs
+++ icream/Models/icreamContext.cs
@@ -33,7 +33,7 @@ namespace icream.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=DESKTOP-U3NUL1T\\SQLEXPRESS;Initial Catalog=icream;Integrated Security=True;Encrypt=False");
+                optionsBuilder.UseSqlServer("Data Source=icreamdb.mssql.somee.com;user id=yousofkortam_SQLLogin_1;pwd=qbnd39ai6q;Initial Catalog=icreamdb;Encrypt=False");
             }
         }
 
